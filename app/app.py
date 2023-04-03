85d47b424383a7f7146ea74fbb467dcd0a6c2cac
breidi28 <v.bradiceanu@student.fontys.nl>
2023-04-03 11:42:25 +0300

    improved tne app.py

--- app/app.py
+++ app/app.py
@@ -7,5 +7,25 @@ app.route('/index')
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/pickup')
+def pickup():
+    return render_template('pickup.html')
+
+@app.route('/delivery')
+def delivery():
+    return render_template('delivery.html')
+
+@app.route('/deliverypostscreen')
+def deliverypostscreen():
+    return render_template('deliverypostscreen.html')
+
+@app.route('/pickuppostscreen')
+def pickuppostscreen():
+    return render_template('pickuppostscreen.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
