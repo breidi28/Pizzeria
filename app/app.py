433b7f9b388640ee0aba69bc583829f0858b0df8
breidi28 <v.bradiceanu@student.fontys.nl>
2023-04-04 11:03:17 +0300

    pizza added and flask css added

--- app/app.py
+++ app/app.py
@@ -2,30 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-app.route('/')
-app.route('/index')
+@app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/mario')
+def mario():
+    return render_template('mario.html')
 
-@app.route('/pickup')
-def pickup():
-    return render_template('pickup.html')
-
-@app.route('/delivery')
-def delivery():
-    return render_template('delivery.html')
-
-@app.route('/deliverypostscreen')
-def deliverypostscreen():
-    return render_template('deliverypostscreen.html')
-
-@app.route('/pickuppostscreen')
-def pickuppostscreen():
-    return render_template('pickuppostscreen.html')
+@app.route('/luigi')
+def luigi():
+    return render_template('luigi.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
