3e4db0f688645ba706ae664e87860ab24aacd48d
breidi28 <vladbradiceanuandrei@gmail.com>
2023-04-05 09:40:26 +0300

    small tweaaks

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import pandas as pd
+import requests
 
 app = Flask(__name__)
 
@@ -9,6 +11,14 @@ def index():
 
 @app.route('/mario')
 def mario():
+    """This function will take the order from mario.html and send it to the server.
+    It will also send the data to luigi.html to queue the orders on the dashboard."""
+    # when a user chooses a pizza or multiple pizzas, the id of the pizza will be sent to the server and added to the order list
+    # the list is then sent to the server to luiigi.html to be displayed on the dashboard
+    # the order list will be a list of dictionaries
+    # each dictionary will have the pizza name, id, and time to make
+    order_list = []
+    
     return render_template('mario.html')
 
 @app.route('/luigi')
