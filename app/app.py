a08df790c71c954817c4994bb0e413ed26596574
breidi28 <vladbradiceanuandrei@gmail.com>
2023-04-05 11:53:00 +0300

    added the luigi page and the interaction between mario and luigi

--- app/app.py
+++ app/app.py
@@ -1,23 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+orders = []
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
 
+@app.route('/mario.html')
 @app.route('/mario')
 def mario():
     return render_template('mario.html')
 
-@app.route('/luigi')
-def luigi():
-    return render_template('luigi.html')
-
 @app.route('/submit_pizzas', methods=['POST'])
 def submit_pizzas():
-    return render_template('submit_pizzas.html')
+    selected_pizzas = request.form.getlist('pizzas[]')
+    orders.append({"id": len(orders)+1, "selected_pizzas": selected_pizzas})
+    return redirect(url_for('luigi'))
+
+@app.route('/luigi.html')
+@app.route('/luigi')
+def luigi():
+    return render_template('luigi.html', orders=orders)
 
 if __name__ == '__main__':
     app.run(debug=True)
